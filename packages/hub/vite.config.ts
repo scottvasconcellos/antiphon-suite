8aa9ab952d92863c7e0e3671bee4b702feffea7b
Scott Vasky <100307333+scottvasconcellos@users.noreply.github.com>
2026-02-18 19:38:43 -0600

    chore(hub): set base='./' and emptyOutDir for Electron build

--- packages/hub/vite.config.ts
+++ packages/hub/vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: './',
+  base: './',
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
